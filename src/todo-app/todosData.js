d53248914f28ffdd780b947a4eef7908edfeab2f
sayed.musavy@integrify.io <sayed.musavy@integrify.io>
2019-04-30 11:26:04 +0300

    adding a folder to the todos

---
+++ src/todo-app/todosData.js
@@ -0,0 +1,32 @@
+const todosData=[
+
+{
+
+id:1,
+text:"Hey You take out the Trash",
+completed:false
+},
+{
+    id:2,
+    text:"Going to Espoo shopping mall",
+    completed:false
+},
+{
+    id: 3,
+    text: "Clean gecko tank", 
+    completed: false
+},
+{
+    id: 4,
+    text: "Masood", 
+    completed: false
+},
+{
+    id: 5,
+    text: "Catch up on Arrested Development", 
+    completed: false
+}
+
+]
+
+export default todosData;
