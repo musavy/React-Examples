3a83b7b689833ebd2c74197360b80899a96bb8e6
sayed.musavy@integrify.io <sayed.musavy@integrify.io>
2019-04-30 12:00:35 +0300

    adding some style and some memes folders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import TodoApp from "./todo-app/App";
+import MemeApp from "./meme-generator.js/App"
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/meme-generator.js/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./Header";
+import MemMemeGenerator from "./MemeGenerator";
+
+
+function App(){
+    return (
+        <div>
+<Header />
+<MemMemeGenerator />
+        </div>
+    )
+}
+
+export default App;
